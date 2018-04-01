a6008159ef3156853b9dd0efbece0c886b5b65c7
Feng Li <lifcn@yahoo.com>
2018-04-01 11:04:45 +1000

    Add basic auth - test login.

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 ## Versions
-**Version v0.0.1**
+**Version 0.1.1**
 
 ## Description
 - [x] Fresh install of Laravel Framework 5.5.40.
 - [x] Set up Laravel Dusk Browser test.
 - [x] Change website title.
 - [x] Add basic auth - test register.
+- [x] Add basic auth - test login.
--- tests/Browser/BasicAuthTest.php
+++ tests/Browser/BasicAuthTest.php
@@ -6,9 +6,28 @@ use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
+use App\User;
+
 class BasicAuthTest extends DuskTestCase
 {
     use DatabaseMigrations;
+
+    private $user;
+
+    protected function setUp() {
+        parent::setUp(); // this is important!
+
+        $this->user = factory(User::class)->create();
+    }
+
+    protected function tearDown() {
+        parent::tearDown(); // this is important!
+
+        // Clear cookies
+        foreach (static::$browsers as $browser) {
+          $browser->driver->manage()->deleteAllCookies();
+        }
+    } 
 
     public function testRegisterLink()
     {
@@ -45,5 +64,27 @@ class BasicAuthTest extends DuskTestCase
                     ->assertSee("You are logged in!");
         });
     }
+
+     public function testLogin()
+     {
+         //var_dump($user->email);
+         $this->user->email = 'lifcn@yahoo.com';
+         $this->user->password = bcrypt('test01pass');  
+         $this->user->save();
+
+         $this->browse(function ($browser) {
+             $browser->visit('/login')
+                     ->value('#email', $this->user->email)
+                     ->value('#password', 'test01pass')
+                     ->press('Login')
+                     ->assertPathIs('/home')
+                     ->assertSee("You are logged in!");
+         });
+
+         // Tear Down
+         // Delete test user
+         User::where(['email' => $this->user->email])->delete();
+     }
+
 
 }
