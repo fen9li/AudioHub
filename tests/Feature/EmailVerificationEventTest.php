03e761e05ebcb9e3e3d066a7b1babd9192e15fed
Feng Li <lifcn@yahoo.com>
2018-04-12 12:40:56 +1000

    Create feature tests for UserRegisteredEvent and its listeners.

--- tests/Feature/EmailVerificationEventTest.php
+++ tests/Feature/EmailVerificationEventTest.php
@@ -8,8 +8,10 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 use App\Events\UserRegisteredEvent;
 use App\Listeners\SaveEmailVerificationToken;
+use App\Notifications\EmailVerificationNotification;
 use App\User;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class EmailVerificationEventTest extends TestCase
@@ -17,41 +19,53 @@ class EmailVerificationEventTest extends TestCase
 
     use RefreshDatabase;
 
+    private $user;
+
+    protected function setUp() {
+        parent::setUp(); 
+
+        $this->user = factory(User::class)->create();
+    }
+
     /**
      * test user registration can dispatch an UserRegisteredEvent.
-     * test UserRegisteredEvent can trigger SaveEmailVerificationToken
-     * test UserRegisteredEvent can trigger SendEmailVerificationNotification
      * @return void
      */
-    public function testSaveEmailVerificationTokenListener()
+    public function testUserRegisteredEventDispatched()
     {
-        Event::fake();
+        Event::fake();  // dont trigger listeners
 
-        // performing user registration
-        User::create([
-            'name' => 'Feng Li',
-            'email'      => 'lifcn@yahoo.com',
-            'password'   => bcrypt('password'),
-        ]);
+        $user = $this->user;
+        event(new UserRegisteredEvent($user));
 
-        $user = User::where('email', 'lifcn@yahoo.com')->first();
-        //dd($user);
-
-        $this->assertDatabaseHas('users',[
-            'email' => 'lifcn@yahoo.com',
-            'is_verified' => 0,
-        ]); 
-
-        $this->assertDatabaseHas('email_verifications',[
-            'email' => 'lifcn@yahoo.com',
-        ]);
-// the assertDispatched doesnt work
-// check the source code in
-// vendor/laravel/framework/src/Illuminate/Support/Testing/Fakes/EventFake.php
-// leave this as is at this moment
-//        Event::assertDispatched(UserRegisteredEvent::class, function ($e) use ($user) {
-//            return $e->user->id === $user->id;
-//        });
-
+        // assert event is dispatched againest right user
+        Event::assertDispatched(UserRegisteredEvent::class, function ($e) use ($user) {
+            return $e->user->id === $user->id;
+        });
+    
+        // assert event is dispatch 1 time only
+        Event::assertDispatched(UserRegisteredEvent::class, 1);
     }
+
+    /**
+     * test UserRegisteredEvent can trigger 'SaveEmailVerificationToken' Listener
+     * test UserRegisteredEvent can trigger 'SendEmailVerificationNotification' Listener
+     * @return void
+     */
+    public function testListeners()
+    {
+        Notification::fake();
+
+        $user = $this->user;
+        //dd($user);
+        // fire UserRegisteredEvent
+        event(new UserRegisteredEvent($user));
+
+        $this->assertDatabaseHas('email_verifications',['email' => $user->email]);       
+
+        Notification::assertSentTo(
+            [$user], EmailVerificationNotification::class
+        );
+    }
+
 }
